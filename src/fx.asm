576e3b2f523879db5b06976b3e791fd7b04f8d91
Florent Flament <contact@florentflament.com>
2020-10-13 17:50:13 +0200

    Modify note fetcher - avoids updating TT state

--- src/fx.asm
+++ src/fx.asm
@@ -1,27 +1,37 @@
+    MAC GET_NOTE
+        ldy tt_cur_pat_index_c0,x       ; get current pattern (index into tt_SequenceTable)
+        lda tt_SequenceTable,y
+        tay
+        lda tt_PatternPtrLo,y
+        sta tt_ptr
+        lda tt_PatternPtrHi,y
+        sta tt_ptr+1
+        ldy tt_cur_note_index_c0,x
+        lda (tt_ptr),y
+    ENDM
+
 fx_init:
         lda #$ff
         sta COLUPF
 	rts
 
-fx_fetch_current_note:
-        TT_FETCH_CURRENT_NOTE
-        rts
-
 fx_vblank:
         lda tt_timer
         cmp #TT_SPEED-1
         bne .end_vblank
+
+        ;; Search rewind ball if starting a drum
         ldx #1
-        jsr fx_fetch_current_note
+        GET_NOTE
         cmp #$08                ; Silence
         beq .end_vblank
         and #$e0                ; Cut frequency
         cmp #$0                 ; Percussion
-        ;cmp #$60                ; instrument
         bne .end_vblank
 .note_found:
         lda #$ff
         sta posy
+
 .end_vblank:
         inc posy
 	rts

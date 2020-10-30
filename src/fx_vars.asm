d6c8458f697896f48aee7818c6f64c2c18ffea6c
Florent Flament <contact@florentflament.com>
2020-10-30 17:50:37 +0100

    Change stack_idkern to stack_ikern in fx_vars as well

--- src/fx_vars.asm
+++ src/fx_vars.asm
@@ -1,7 +1,7 @@
 STACK_SIZE      = 48
 
 stack_idx       DS.B    1       ; Stack index for pushing notes
-stack_idkern    DS.B    1       ; Kernel stack index for display
+stack_ikern     DS.B    1       ; Kernel stack index for display
 cur_note_c0     DS.B    1
 cur_note_c1     DS.B    1
 
